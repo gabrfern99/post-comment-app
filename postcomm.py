2512d115d325ecbb2de133f6565908dfa6df26bc
cr1sk <gabr.fern99@gmail.com>
2023-06-18 14:40:06 -0300

    Create database models

--- postcomm.py
+++ postcomm.py
@@ -7,3 +7,25 @@ app.config['SECRET_KEY'] = 'teste123'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///postcomm.db'
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+
+
+# Create database models
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True)
+    password = db.Column(db.String(20))
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post = db.relationship('Post', backref=db.backref('comments', lazy=True))
+    user = db.relationship('User', backref=db.backref('comments', lazy=True))
+
